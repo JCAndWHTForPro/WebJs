f3fcf58428596c72f532615f51d03718411795f4
纪成10192096 <ji.cheng@zte.com.cn>
2017-04-20 10:05:59 +0800

    整理文件夹，删除废弃文件

--- pygame_first/game_stats.py
+++
@@ -1,9 +0,0 @@
-class GameStats(object):
-    def __init__(self, ai_settings):
-        super(GameStats, self).__init__()
-        self.ai_settings = ai_settings
-        self.reset_stats()
-        self.game_active = True
-
-    def reset_stats(self):
-        self.ships_left = self.ai_settings.ship_limit
